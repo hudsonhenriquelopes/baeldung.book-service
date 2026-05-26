1f6cb24f6ccb8478052262a6b731074c59b3dc54
Hudson Henrique Lopes <hudsonhenriquelopes@gmail.com>
2026-05-25 21:02:53 -0300

    Fix issues

--- src/main/java/com/hch/bookservice/BookServiceApplication.java
+++ src/main/java/com/hch/bookservice/BookServiceApplication.java
@@ -11,7 +11,7 @@ import java.util.Arrays;
 import java.util.List;
 
 @SpringBootApplication
-//@EnableDiscoveryClient
+@EnableDiscoveryClient(autoRegister = true)
 @RestController
 public class BookServiceApplication {
 
